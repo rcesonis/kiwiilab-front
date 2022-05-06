eea6828dfc01833e9dd9229aeebb0f29618d549d
“rcesonis” <rimvydas.cesonis@gmail.com>
2022-05-06 12:06:37 +0200

    Fixed

--- src/components/FeaturedProduct/FeaturedProduct.jsx
+++ src/components/FeaturedProduct/FeaturedProduct.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Button } from "react-bootstrap";
 import { apiUrl } from "../../config/constants";
 import { Link } from "react-router-dom";
 import "./FeaturedProduct.scss";
